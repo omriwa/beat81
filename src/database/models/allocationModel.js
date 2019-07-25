fdba684fc3517488fddf86edfd12f717e81a4e96
omri wallach <omriwallach1992@gmail.com>
2019-07-25 17:15:59 +0200

    add sensor and allocation dbModel

---
+++ src/database/models/allocationModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const allocationSchema = new Schema({
+    user_id: Schema.Types.ObjectId,
+    sensor_id: { Schema.Types.ObjectId, ref: 'Sensor'},
+    sensor_is_user_property: Boolean,
+    created_at: Date,
+    updated_at: Date
+});
+
+const AllocationModel = mongoose.model('Allocation', allocationSchema);
+
+module.exports = { AlloctionModel };
---
+++ src/database/models/sensorModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sensorSchema = new Schema({
+    sensorId: Schema.Types.ObjectId,
+    is_allocatable: Boolean
+});
+
+const SensorModel = mongoose.model('Sensor', sensorSchema);
+
+module.exports = { SensorModel };
